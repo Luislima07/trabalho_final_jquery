c26a28273347f1d133b51c1afdf1c515d857fdd8
Gustavo_lima07 <luis.lima133@etec.sp.gov.br>
2025-03-25 20:16:31 -0300

    Trabalho - Finalizado

--- js/trabalho.js
+++ js/trabalho.js
@@ -1,22 +1,28 @@
-$("#calcular").click(function(){
+$("#calcular").click(function () {
     var valor1 = parseInt($("#txtvalor1").val())
     var valor2 = parseInt($("#txtvalor2").val())
     var valor3 = parseInt($("#txtvalor3").val())
 
-    ordem = valor1, ",", valor2, ",", valor3
-    media = (valor1 + valor2 + valor3)/3
+    var ordem = [valor1, valor2, valor3].sort(function(a, b) {
+        return a - b;
+    });
+    media = (valor1 + valor2 + valor3) / 3
 
+    var loop = 1
+    for (var x = 0; x < valor2; x++){
+        loop += x
+    }
     $("#ordem")
         .text("Ordem crescente dos valores: " + ordem)
-        .css({ 'font-weight': 'bold', 'font-size': '18pt', 'color': 'orange'})
+        .css({ 'font-weight': 'bold', 'font-size': '18pt', 'color': 'orange' })
 
 
     $("#media")
         .text("A Média dos valores é: " + media)
-        .css({ 'font-weight': 'bold', 'font-size': '18pt', 'color': 'blue'})
+        .css({ 'font-weight': 'bold', 'font-size': '18pt', 'color': 'blue' })
+
+    $("#loop")
+        .text("Os valores usados na 2° caixa são: " + loop)
+        .css({ 'font-weight': 'bold', 'font-size': '18pt', 'color': 'blue' })
     
-    var loop = valor1 + valor2 + valor3
-    for(var x = 1; x <= loop; x++){
-        loop += lopp + x
-    }
 });
